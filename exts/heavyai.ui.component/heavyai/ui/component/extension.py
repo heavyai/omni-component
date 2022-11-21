1caa300ead32405e77357a5f03691085635d27fd
Brendan Turner <brendan.turner@heavy.ai>
2022-11-21 11:33:55 -0600

    document component

--- exts/heavyai.ui.component/heavyai/ui/component/extension.py
+++ exts/heavyai.ui.component/heavyai/ui/component/extension.py
@@ -10,6 +10,25 @@ if TYPE_CHECKING:
 
 
 class Component:
+    """
+    The base class that UI elements should be subclassed from
+
+    Attributes
+    ----------
+    name : Optional[str]
+        The name of the root container.
+    style : Optional[Dict]
+        The local style of the root container.
+    height : Optional[int]
+        The height of the root container.
+    width : Optional[int]
+        The width of the root container
+    style_type_name_override : Optional[str]
+        By default, we use typeName to look up the style.
+        But sometimes it's necessary to use a custom name.
+        For example, when a widget as a part of another widget.(Label is a part of Button)
+        This property can override the name to use in style.
+    """
     style: Optional[Dict] = None
     height: Optional[int] = None
     width: Optional[int] = None
@@ -17,6 +36,12 @@ class Component:
     style_type_name_override: Optional[str] = None
 
     def __init__(self, render_on_init=True, **kwargs):
+        """
+        Parameters
+        ----------
+        render_on_init : bool
+            If the render method should be called upon component creation
+        """
         # ui.Container is ui.VStack/HStack/ZStack/etc
         self._root: ui.Container = None
         self._debounce_task: asyncio.Future = None
@@ -67,6 +92,12 @@ class Component:
             self._root.enabled = value
 
     def get_root(self, Container: ui.Container, default_visible=True, **kwargs):
+        """
+        Creates and returns a new container upon initial call.
+
+        Clears the container and returns reference upon subsequent calls.
+        This allows a component to be re-rendered without losing its positioning
+        """
         if self._root:
             self._root.clear()
         else:
@@ -85,13 +116,18 @@ class Component:
         return self._root
 
     async def render_async(self):
+        """Waits for next frame before re-rendering"""
         await omni.kit.app.get_app().next_update_async()
         self.render()
 
     def update(self, loop=asyncio.get_event_loop()):
+        """Used to re-render the component"""
         asyncio.ensure_future(self.render_async(), loop=loop)
 
     def update_debounce(self, delay=0.2):
+        """
+        Queues re-render after a delay and resets the timer on subsequent calls if timer has not completed
+        """
         async def run_after_delay():
             await asyncio.sleep(delay)
             await self.render_async()
@@ -105,7 +141,17 @@ class Component:
         raise NotImplementedError()
 
     def __del__(self):
+        """
+        Note: `__del__` is not reliably called when parent component is destroyed or re-rendered
+
+        If a component requires clean-up (such as subscriptions, windows, frames, or event listeners),
+        the parent component/class must manually call destroy when appropriate.
+        """
         self.destroy()
 
     def destroy(self):
+        """
+        If a component requires clean-up (such as subscriptions, windows, frames, or event listeners),
+        the parent component/class must manually call destroy when appropriate.
+        """
         pass
